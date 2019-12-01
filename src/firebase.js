51a50a0e11f3816e875cb9b9f658439cc49012b3
Liam Rowley <liam.rowley@hotmail.co.uk>
2019-12-01 21:30:17 +0000

    Change firebase for heroku deploy

--- src/firebase.js
+++ src/firebase.js
@@ -6,9 +6,7 @@ import 'firebase/auth'
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  ...process.env.FIREBASE_CONFIG,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID
 };
