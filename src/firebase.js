1cb55d71180356f8564ac36c45a02dace88fd4f3
Liam Rowley <liam.rowley@hotmail.co.uk>
2019-12-01 21:34:53 +0000

    Heroku

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth'
+import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  ...process.env.FIREBASE_CONFIG,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyBM5VJSN4b3NsTJiDYsA3yga2BjENeglmw",
+  authDomain: "todoit-d08f4.firebaseapp.com",
+  databaseURL: "https://todoit-d08f4.firebaseio.com",
+  projectId: "todoit-d08f4",
+  storageBucket: "todoit-d08f4.appspot.com",
+  messagingSenderId: "62582518292",
+  appId: "1:62582518292:web:25aa863f972ef635e16d01"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
